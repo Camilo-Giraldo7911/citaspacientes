9e1ebde6e3b08d301c6a5350d09ff2ccccee6a9c
dcamigira11 <dcamigira11@gmail.com>
2020-04-24 17:41:49 -0500

    segundocommit

---
+++ client/build/precache-manifest.792d2c1e690c12add5a8ea1f490e5954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee0d45c7bfe5967468d1686a193aeeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd2e166480d600a0998",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "cf799195a9a2acadceb2",
+    "url": "/static/js/2.67b51cf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67b51cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd2e166480d600a0998",
+    "url": "/static/js/main.5d098234.chunk.js"
+  },
+  {
+    "revision": "32395f8a6bb214f0c77c",
+    "url": "/static/js/runtime-main.f89b289e.js"
+  }
+]);
